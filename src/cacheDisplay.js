6ac9d5fdb10e0a54a948cd5f134f4fe375625a66
emestanza3 <eduardomestanza_96@gatech.edu>
2017-10-03 22:57:45 -0400

    Dynamic change of cache sizes on canvas

--- src/cacheDisplay.js
+++ src/cacheDisplay.js
@@ -25,21 +25,100 @@ function CacheDisplayController($scope, simDriver, fileParser) {
 
     ctrl.caches = [{
         title: "L1",
-        policy: "",
-        cacheSize: "",
-        associativity: "",
-        C: "",
-        S: ""
-    }];
-
-    ctrl.caches = [{
-        title: "L1",
         size: "Not Set",
         associativity: "Not Set",
         associativities: [],
         C: 1,
         S: 1
     }];
+
+    //Display of the canvas for the general overview
+    //canvas1 holds L1 and L2, changes to the display
+    //of these caches are to be done to L1 and L2 directly
+    //canvas2 has L3 in it and changes to the cache display
+    //will be done directly to L3
+    var canvas1 = document.getElementById("canvas1");
+    canvas1.style.flex = "auto";
+
+    var canvas2 = document.getElementById("canvas2");
+    canvas2.style.flex = "";
+
+    var l1 = document.getElementById("L1");
+    l1.style.flex = 1;
+    var l2 = document.getElementById("L2");
+    l2.style.flex = 1;
+    var l3 = document.getElementById("L3");
+    l3.style.flex = 1;
+
+    $scope.showL3 = function() {
+        canvas2.style.flex = "auto";
+        canvas2.style.height = "40%";
+    }
+
+    $scope.updateCacheCanvas = function() {
+        if (ctrl.caches.length == 1) {
+            l1.style.flex = 1;
+            l2.style.flex = 1;
+            canvas2.style.flex = "";
+            canvas2.style.height = "0%"; 
+        }
+        if (ctrl.caches.length == 2) {
+            canvas2.style.flex = "";
+            canvas2.style.height = "0%"; 
+            if (ctrl.caches[0].size != "Not Set" && ctrl.caches[1].size != "Not Set") {
+                var l1Size = ctrl.caches[0].size; 
+                var l2Size = ctrl.caches[1].size;
+                var ratio = 0;
+                ratio = l2Size / l1Size;
+                if (ratio < 0.3) {
+                    ratio = 0.3;
+                } else if (ratio > 3) {
+                    ratio = 3
+                }
+                l1.style.flex = 1;
+                l2.style.flex = ratio;
+            }
+        }
+        if (ctrl.caches.length == 3) {
+            if (ctrl.caches[0].size != "Not Set" && ctrl.caches[1].size != "Not Set" && ctrl.caches[1].size == "Not Set") {
+                var l1Size = ctrl.caches[0].size; 
+                var l2Size = ctrl.caches[1].size;
+                var ratio = l2Size / l1Size;
+                if (ratio < 0.3) {
+                    ratio = 0.3;
+                } else if (ratio > 3) {
+                    ratio = 3
+                }
+                
+                l1.style.flex = 1;
+                l2.style.flex = ratio;
+            } else if (ctrl.caches[0].size != "Not Set" && ctrl.caches[1].size != "Not Set" && ctrl.caches[1].size != "Not Set") {
+                canvas2.style.flex = "";
+                var l1Size = ctrl.caches[0].size; 
+                var l2Size = ctrl.caches[1].size;
+                var l3Size = ctrl.caches[2].size;
+                var canvas1Ratio = l2Size / l1Size;
+                if (canvas1Ratio < 0.3) {
+                    canvas1Ratio = 0.3;
+                } else if (canvas1Ratio > 3) {
+                    canvas1Ratio = 3
+                }
+                var ratioSize = parseInt(l1Size) + parseInt(l2Size);
+                var canvas2Ratio = l3Size / ratioSize;
+
+                canvas2Ratio = canvas2Ratio * 40;
+                l1.style.flex = 1;
+                l2.style.flex = canvas1Ratio;
+                if (canvas2Ratio < 30) {
+                    canvas2Ratio = 30;
+                }
+                if (canvas2Ratio > 90) {
+                    canvas2Ratio = 90;
+                }
+                canvas2.style.height = canvas2Ratio.toString() + "%";
+            }
+        }
+    }
 
     ctrl.hideSideBar = function() {
         ctrl.hide = true;
@@ -58,9 +137,12 @@ function CacheDisplayController($scope, simDriver, fileParser) {
             ctrl.caches.push({
                 title: "L" + (ctrl.caches.length + 1),
                 size: "Not Set",
-                associativity: "Not Set"
+                associativity: "Not Set",
             });
             $scope.showCache[ctrl.caches.length - 1] = true;
+            if (ctrl.caches.length == 3) {
+                $scope.showL3();
+            }
             //Emit sends an event to the parent controller/component
             $scope.$emit('updatedCacheList', ctrl.caches);
         }
@@ -83,6 +165,7 @@ function CacheDisplayController($scope, simDriver, fileParser) {
             if (ctrl.caches.length === 1) {
                 ctrl.disableDeleteCache = true;
             }
+            $scope.updateCacheCanvas();
             $scope.$emit('updatedCacheList', ctrl.caches);
         }
     };
@@ -147,6 +230,7 @@ function CacheDisplayController($scope, simDriver, fileParser) {
         } else if (setting === "associativity") {
             c.associativity = item;
         }
+        $scope.updateCacheCanvas();
     }
     
     for (var i = B_min; i <= B_max; i++) {
