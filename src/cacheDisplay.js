e2950f147c2465884a93bd89e4f5521c6ab69289
rberst <rberst@my.waketech.edu>
2017-10-04 10:11:23 -0400

    Made code EMCA 6 compliant

--- src/cacheDisplay.js
+++ src/cacheDisplay.js
@@ -40,39 +40,38 @@ function CacheDisplayController($scope, simDriver, fileParser) {
     //of these caches are to be done to L1 and L2 directly
     //canvas2 has L3 in it and changes to the cache display
     //will be done directly to L3
-    var canvas1 = document.getElementById("canvas1");
+    let canvas1 = document.getElementById("canvas1");
     canvas1.style.flex = "auto";
 
-    var canvas2 = document.getElementById("canvas2");
+    let canvas2 = document.getElementById("canvas2");
     canvas2.style.flex = "";
 
-    var l1 = document.getElementById("L1");
+    let l1 = document.getElementById("L1");
     l1.style.flex = 1;
-    var l2 = document.getElementById("L2");
+    let l2 = document.getElementById("L2");
     l2.style.flex = 1;
-    var l3 = document.getElementById("L3");
+    let l3 = document.getElementById("L3");
     l3.style.flex = 1;
 
     $scope.showL3 = function() {
         canvas2.style.flex = "auto";
         canvas2.style.height = "40%";
-    }
+    };
 
     $scope.updateCacheCanvas = function() {
-        if (ctrl.caches.length == 1) {
+        if (ctrl.caches.length === 1) {
             l1.style.flex = 1;
             l2.style.flex = 1;
             canvas2.style.flex = "";
             canvas2.style.height = "0%";
         }
-        if (ctrl.caches.length == 2) {
+        if (ctrl.caches.length === 2) {
             canvas2.style.flex = "";
             canvas2.style.height = "0%";
-            if (ctrl.caches[0].size != "Not Set" && ctrl.caches[1].size != "Not Set") {
-                var l1Size = ctrl.caches[0].size;
-                var l2Size = ctrl.caches[1].size;
-                var ratio = 0;
-                ratio = l2Size / l1Size;
+            if (ctrl.caches[0].size !== "Not Set" && ctrl.caches[1].size !== "Not Set") {
+                let l1Size = ctrl.caches[0].size;
+                let l2Size = ctrl.caches[1].size;
+                let ratio = l2Size / l1Size;
                 if (ratio < 0.3) {
                     ratio = 0.3;
                 } else if (ratio > 3) {
@@ -82,11 +81,11 @@ function CacheDisplayController($scope, simDriver, fileParser) {
                 l2.style.flex = ratio;
             }
         }
-        if (ctrl.caches.length == 3) {
-            if (ctrl.caches[0].size != "Not Set" && ctrl.caches[1].size != "Not Set" && ctrl.caches[1].size == "Not Set") {
-                var l1Size = ctrl.caches[0].size;
-                var l2Size = ctrl.caches[1].size;
-                var ratio = l2Size / l1Size;
+        if (ctrl.caches.length === 3) {
+            if (ctrl.caches[0].size !== "Not Set" && ctrl.caches[1].size !== "Not Set" && ctrl.caches[1].size === "Not Set") {
+                let l1Size = ctrl.caches[0].size;
+                let l2Size = ctrl.caches[1].size;
+                let ratio = l2Size / l1Size;
                 if (ratio < 0.3) {
                     ratio = 0.3;
                 } else if (ratio > 3) {
@@ -95,19 +94,19 @@ function CacheDisplayController($scope, simDriver, fileParser) {
 
                 l1.style.flex = 1;
                 l2.style.flex = ratio;
-            } else if (ctrl.caches[0].size != "Not Set" && ctrl.caches[1].size != "Not Set" && ctrl.caches[1].size != "Not Set") {
+            } else if (ctrl.caches[0].size !== "Not Set" && ctrl.caches[1].size !== "Not Set" && ctrl.caches[1].size !== "Not Set") {
                 canvas2.style.flex = "";
-                var l1Size = ctrl.caches[0].size;
-                var l2Size = ctrl.caches[1].size;
-                var l3Size = ctrl.caches[2].size;
-                var canvas1Ratio = l2Size / l1Size;
+                let l1Size = ctrl.caches[0].size;
+                let l2Size = ctrl.caches[1].size;
+                let l3Size = ctrl.caches[2].size;
+                let canvas1Ratio = l2Size / l1Size;
                 if (canvas1Ratio < 0.3) {
                     canvas1Ratio = 0.3;
                 } else if (canvas1Ratio > 3) {
                     canvas1Ratio = 3
                 }
-                var ratioSize = parseInt(l1Size) + parseInt(l2Size);
-                var canvas2Ratio = l3Size / ratioSize;
+                let ratioSize = parseInt(l1Size) + parseInt(l2Size);
+                let canvas2Ratio = l3Size / ratioSize;
 
                 canvas2Ratio = canvas2Ratio * 40;
                 l1.style.flex = 1;
@@ -121,7 +120,7 @@ function CacheDisplayController($scope, simDriver, fileParser) {
                 canvas2.style.height = canvas2Ratio.toString() + "%";
             }
         }
-    }
+    };
 
     ctrl.hideSideBar = function () {
         ctrl.hide = true;
