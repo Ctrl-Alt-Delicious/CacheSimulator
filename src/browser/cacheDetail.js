4b7f8b8a08e9f9e0c0a31e43577a07ae9b1acbc7
Will <wthompson40@gatech.edu>
2017-10-20 22:15:14 -0400

    TODO - retrieve associativity from cacheInfo

--- src/browser/cacheDetail.js
+++ src/browser/cacheDetail.js
@@ -10,10 +10,16 @@ function CacheDetailController($scope) {
 
     let ctrl = this;
 
+    ctrl.cacheInfo = $scope.$parent.initialCacheInfo;
+
     ctrl.activeCache = 'L1';
 
     $scope.$on('updatedNavs', function(event, data) {
         ctrl.activeCache = data.buttonTitle;
+    });
+
+    $scope.$on('cacheInfoUpdated', function(event, data) {
+        ctrl.cacheInfo = data;
     });
 
 }
