daa2f18f88bb4a85885f7e65461cfff49f90307a
Will Thompson <wthompson40@gatech.edu>
2017-10-22 16:56:00 -0400

    show address, tag, index, offset in cache details page (#78)

--- src/browser/cacheDetail.js
+++ src/browser/cacheDetail.js
@@ -6,18 +6,19 @@ angular.module('Simulator').component('cacheDetail', {
     bindings: {}
 });
 
-function CacheDetailController($scope) {
+function CacheDetailController($scope, SimDriver) {
 
     let ctrl = this;
 
     ctrl.cacheInfo = $scope.$parent.initialCacheInfo;
 
     ctrl.activeCache = 'L1';
-
+    ctrl.maq = [];
     ctrl.index = 0;
 
     $scope.$on('updatedNavs', function(event, data, index) {
         ctrl.activeCache = data.buttonTitle;
+        ctrl.maq = SimDriver.setQueue(SimDriver.getMemAcceses());
         ctrl.index = index;
     });
 
@@ -28,5 +29,4 @@ function CacheDetailController($scope) {
     ctrl.getAssociativity = function(event, index) {
         return parseInt(ctrl.cacheInfo.caches[ctrl.index].associativity);
     }
-
 }
